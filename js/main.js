2a731aaa5c093d46818a8810bc954487b48d4236
Khenysson <khenysson@hotmail.com>
2020-03-09 12:11:14 -0300

    Arquivos iniciais

---
+++ js/main.js
@@ -0,0 +1,28 @@
+setInterval(() => {
+const difference = new Date("2020-03-16") - new Date();
+const parts = {
+    D: Math.floor(difference / (1000 * 60 * 60 * 24)),
+    H: Math.floor((difference / (1000 * 60 * 60)) % 24),
+    M: Math.floor((difference / 1000 / 60) % 60),
+    S: Math.floor((difference / 1000) % 60),
+  };
+
+  
+  const remaining = Object.keys(parts)
+  .map((part) => {
+    if (!parts[part]) return;
+    return `${parts[part]}${part}`;
+  })
+  .join(" ");
+
+  document.getElementById("countdown").innerHTML = remaining;
+
+
+    // This is where we'd recalculate the time remaining
+  }, 1000);
+
+      
+     
+
+  
+  
