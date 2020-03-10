73cd74a45d2664a0a04f25d05813b204e7dc1056
Khenysson <khenysson@hotmail.com>
2020-03-09 23:31:52 -0300

    Alterações Diversas

--- js/main.js
+++ js/main.js
@@ -1,38 +1,51 @@
 setInterval(() => {
-const difference = new Date("2020-03-16") - new Date();
-const parts = {
-    D: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    H: Math.floor((difference / (1000 * 60 * 60)) % 24),
-    M: Math.floor((difference / 1000 / 60) % 60),
-    S: Math.floor((difference / 1000) % 60),
-  };
-
-  
-  const remaining = Object.keys(parts)
-  .map((part) => {
-    if (!parts[part]) return;
-    return `${parts[part]}${part}`;
-  })
-  .join(" ");
-
-  document.getElementById("countdown").innerHTML = remaining;
+	const difference = new Date("2020-03-16") - new Date();
+	const parts = {
+		D: Math.floor(difference / (1000 * 60 * 60 * 24)),
+		H: Math.floor((difference / (1000 * 60 * 60)) % 24),
+		M: Math.floor((difference / 1000 / 60) % 60),
+		S: Math.floor((difference / 1000) % 60),
+	};
 
 
-    // This is where we'd recalculate the time remaining
-  }, 1000);
+	const remaining = Object.keys(parts)
+		.map((part) => {
+			if (!parts[part]) return;
+			return `${parts[part]}${part}`;
+		})
+		.join(" ");
+
+	document.getElementById("countdown").innerHTML = remaining;
+
+
+	// This is where we'd recalculate the time remaining
+}, 1000);
 
 
 // FAQ
-  const items = document.querySelectorAll(".accordion a");
+const items = document.querySelectorAll(".accordion a");
 
-  function toggleAccordion(){
-    this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('active');
-  }
-  
-  items.forEach(item => item.addEventListener('click', toggleAccordion));
-      
-     
+function toggleAccordion() {
+	this.classList.toggle('active');
+	this.nextElementSibling.classList.toggle('active');
+}
 
-  
-  
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+  $("#alterarValorDecrescente").click(function(){
+    //Valor inicial
+    var count = 823;
+    countdown = setInterval(function(){
+      $('#contagem').html(count);
+      //Valor final
+      if (count == 1){
+        //Ao chegar no valor final (1) interrompe a contagem
+        clearInterval(countdown);
+      }
+      count--;
+      //Transição a cada meio segundo
+    }, 10000);
+  });
+
+
+$('#alterarValorDecrescente').trigger('click');
